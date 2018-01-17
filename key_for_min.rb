6d2e1ee7749f45fbef572ec6fa21edf346dce936
Melissa Sames <melissa.sames@gmail.com>
2018-01-17 04:28:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = Float::INFINITY
 
-  name_hash.each do { |key, value|
+  name_hash.each { |key, value|
     if value < lowest_value
       lowest_value = value
       lowest_key = key
